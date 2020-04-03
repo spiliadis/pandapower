942e815cb726f9204efbf146175fa42b546f207a
spiliadis <38661383+spiliadis@users.noreply.github.com>
2020-04-03 20:23:42 +0200

    Update newtonpf.py

--- pandapower/pypower/newtonpf.py
+++ pandapower/pypower/newtonpf.py
@@ -155,7 +155,7 @@ def _evaluate_Fx(Ybus, V, Sbus, pv, pq):
     SB = r_[Sbus[pv].real,
             Sbus[pq].real,
             Sbus[pq].imag]
-    APQ = PQx
+    APQ = r_[PQx]
     return F, PQ, SB, APQ
 
 
