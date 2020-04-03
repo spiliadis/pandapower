ad7fed8bff87cee4333073727682467029c2dda5
spiliadis <38661383+spiliadis@users.noreply.github.com>
2020-04-03 21:02:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ long_description = '\n\n'.join((install, changelog))
 
 setup(
     name='pandapower',
-    version='2.2.5.6',
+    version='2.2.5.7',
     author='Leon Thurner, Alexander Scheidler and me',
     author_email='leon.thurner@uni-kassel.de, alexander.scheidler@iee.fraunhofer.de',
     description='Convenient Power System Modelling and Analysis based on PYPOWER and pandas',
